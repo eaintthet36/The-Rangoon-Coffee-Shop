1666409d1aa88023c280e52f466f80c1aa38defb
Eaint Thet <eaintthetpainghmuekhin@gmail.com>
2024-06-18 20:35:26 +0630

    home page and products

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,6 +7,59 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+products =[ {
+        
+            "id": "1",
+            "title": "Chocolate Cappuccino",
+            "description": "Description of Product B",
+            "price": 30,
+            "image": "assets/images/homepage/cappuccino.png"
+        },
+        {
+            "id": "2",
+            "title": "Caramel Latte",
+            "description": "Description of Product B",
+            "price": 29.99,
+            "image": "assets/images/homepage/caramel.png"
+        },
+        {
+          "id": "3",
+          "title": "Mocha Latte",
+          "description": "Description of Product B",
+          "price": 18.99,
+          "image": "assets/images/homepage/mocha.png"
+      },  {
+        "id": "4",
+        "title": "Mocha Latte",
+        "description": "Description of Product B",
+        "price": 29.99,
+        "image": "assets/images/homepage/mocha.png"
+    },
+    {
+      "id": "4",
+      "title": "Mocha Latte",
+      "description": "Description of Product B",
+      "price": 29.99,
+      "image": "assets/images/homepage/mocha.png"
+  },
+  {
+    "id": "2",
+    "title": "Caramel Latte",
+    "description": "Description of Product B",
+    "price": 29.99,
+    "image": "assets/images/homepage/caramel.png"
+},
+{
+  "id": "2",
+  "title": "Caramel Latte",
+  "description": "Description of Product B",
+  "price": 29.99,
+  "image": "assets/images/homepage/caramel.png"
+},
+      ]
+
+
   constructor(private router: Router){}
 
 }
