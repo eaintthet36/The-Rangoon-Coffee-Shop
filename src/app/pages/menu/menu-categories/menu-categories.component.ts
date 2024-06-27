5318531affa81dc377687cbd296913ffef4ab99f
eaintthet36 <eaintthetpainghmuekhin@gmail.com>
2024-06-27 09:57:52 +0630

    got products for menu categories

--- src/app/pages/menu/menu-categories/menu-categories.component.ts
+++ src/app/pages/menu/menu-categories/menu-categories.component.ts
@@ -4,17 +4,18 @@ import { MenuService } from 'src/app/services/menu.service';
 @Component({
   selector: 'app-menu-categories',
   templateUrl: './menu-categories.component.html',
-  styleUrls: ['./menu-categories.component.css']
+  styleUrls: ['./menu-categories.component.css'],
 })
 export class MenuCategoriesComponent implements OnInit {
-  constructor(private menuService: MenuService){};
+  constructor(private menuService: MenuService) {}
   listOfProducts: any[] = [];
 
   selectedButton: string = '';
   ngOnInit(): void {
-    // this.menuService.currentProducts.subscribe(allProducts => {
-    //   this.listOfProducts = allProducts;
-    // });// throw new Error('Method not implemented.');
+    this.menuService.productList.subscribe((allProducts) => {
+      this.listOfProducts = allProducts;
+      console.log(this.listOfProducts, 'shi lr');
+    });
   }
   selectButton(button: string) {
     this.selectedButton = button;
